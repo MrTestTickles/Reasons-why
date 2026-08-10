59357ff5b4872fed5666bbce5ee55a6affe7f650
s260555 <s260555@192.168.1.197>
2026-08-10 18:30:50 -0500

    Intro and Ready Screen

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ let rotation = 0;
 const backbtn = document.getElementById("backbtn");
 const forwardbtn = document.getElementById("forwardbtn");
 
+const introScreen = document.getElementById("introScreen");
+const introScreen = document.getElementById("readyScreen");
+const introScreen = document.getElementById("cardScreen");
+
+const introScreen = document.getElementById("introForward");
+const introScreen = document.getElementById("readtBack");
+const introScreen = document.getElementById("readyForward");
+
 const boxElement = document.querySelector(".box");
 
 backbtn.addEventListener("click", function() { 
@@ -25,6 +33,21 @@ forwardbtn.addEventListener("click", function() {
 		currentIndex = currentIndex + 1;
 		boxElement.textContent = words[currentIndex]}	
 }, 150);
+});
+
+introForward.addEventListener("click", function() {
+	introScreen.style.display = "none";
+	readyScreen.style.display = "flex";
+});
+
+readyBack.addEventListener("click", function() {
+	introScreen.style.display = "none";
+	readyScreen.style.display = "flex";
+});
+
+readyForward.addEventListener("click", function() {
+	introScreen.style.display = "none";
+	readyScreen.style.display = "flex";
 });
 
 boxElement.textContent = words[currentIndex];
