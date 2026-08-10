9091695d488b7a825b439d19475452d586d8777a
s260555 <s260555@192.168.1.197>
2026-08-10 16:03:58 -0500

    Basic smoother animation

--- script.js
+++ script.js
@@ -8,22 +8,31 @@ const boxElement = document.querySelector(".box");
 
 backbtn.addEventListener("click", function() { 
 	boxElement.classList.add("flip")
-	if (currentIndex > 0) {
-	currentIndex = currentIndex - 1;
-	boxElement.textContent = words[currentIndex]}
+	
 	setTimeout(function() {
-	boxElement.classList.remove("flip");			
-	}, 1000);
+		if (currentIndex > 0) {
+		currentIndex = currentIndex - 1;
+		boxElement.textContent = words[currentIndex]}
+},500);
+	
+	setTimeout(function() {
+			 boxElement.classList.remove("flip");		
+			}, 1000);
 });
 
 forwardbtn.addEventListener("click", function() {
 	boxElement.classList.add("flip")
-	if (currentIndex < words.length - 1) {
-	currentIndex = currentIndex + 1;
-	boxElement.textContent = words[currentIndex]}
+	
 	setTimeout(function() {
-	 boxElement.classList.remove("flip");		
-	}, 300);
+		
+		if (currentIndex < words.length - 1) {
+		currentIndex = currentIndex + 1;
+		boxElement.textContent = words[currentIndex]}	
+}, 500);
+	
+	setTimeout(function() {
+		 boxElement.classList.remove("flip");		
+		}, 1000);
 });
 
 boxElement.textContent = words[currentIndex];
